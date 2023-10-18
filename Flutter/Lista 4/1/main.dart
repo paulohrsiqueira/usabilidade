cb36473e8c73bf8f1d070d10c1c2ba425d9388c9
Lucas Garcia Ferreira <90708162+LucasGarcia19@users.noreply.github.com>
2023-10-18 00:30:15 -0300

    Add files via upload

---
+++ Flutter/Lista 4/1/main.dart
@@ -0,0 +1,44 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  int numeroVezes = 0;
+  String mensagem = "Número de vezes em que o botão foi pressionado: 0.\nEssenúmero é par";
+  void cliqueDoBotao() {
+    numeroVezes = numeroVezes + 1;
+    mensagem = "Número de vezes em que o botão foi pressionado: $numeroVezes.\nEsse número é ${numeroVezes % 2 == 0 ? "par" : "impar"}";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Página Inicial"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Número de vezes em que o botão foi pressionado: $numeroVezes.\nEsse número é ${numeroVezes % 2 == 0 ? "par" : "impar"}"),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(cliqueDoBotao);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
